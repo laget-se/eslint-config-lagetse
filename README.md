1dd0a4a7f10888331cf54ae14e1e240eb8640c74
Alexander Wallin <office@alexanderwallin.com>
2016-04-12 11:05:42 +0200

    Initial setup using airbnb config with react and jsx-a11y plugins + some custom rules

---
+++ README.md
@@ -0,0 +1,5 @@
+# eslint-config-lagetse
+
+JavaScript linting rules for laget.se.
+
+Extends [`eslint-config-airbnb`](https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb), which in turn makes use of [`eslint-plugin-react`](https://github.com/yannickcr/eslint-plugin-react) and [`eslint-plugin-jsx-a11y`](https://github.com/evcohen/eslint-plugin-jsx-a11y).
---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    amd: false,
+    commonjs: true,
+    mocha: false,
+    jasmine: false,
+    es6: true
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true
+    }
+  },
+  extends: 'airbnb',
+  plugins: ['react', 'jsx-a11y'],
+  rules: {
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }],
+    'max-len': [2, 120, 2, {'ignoreUrls': true}],
+
+    // When working with words or english phrases as keys, you need to be
+    // able to quote keys, like `'My key': 'My value'`.
+    'quote-props': [2, 'consistent-as-needed'],
+
+    // We allow missing prop types on stateless functional components, but
+    // for shared components, this should be overridden (read in the FEC)
+    'react/prop-types': 0,
+
+    // Provide a way to
+    //
+    // a) import modules that set globals (the colors package for example) and
+    // b) declare a full list of arguments to clarify an API, for example
+    //    const myCallback = (err, _data, _state) { /* only interested in err */ }
+    'no-unused-vars': [2, {
+      'varsIgnorePattern': '[iI]gnored',
+      'argsIgnorePattern': '^_'
+    }]
+  },
+}
